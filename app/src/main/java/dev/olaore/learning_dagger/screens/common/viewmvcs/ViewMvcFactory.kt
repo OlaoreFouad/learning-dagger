0862d8206890bdbb263ce59fbeb8b3d5c5074377
Olaore Fouad <olaorefouad565@gmail.com>
2021-09-06 01:05:31 +0400

    learnt how to use providers to create new instances or scoped dependencies based on the attrs and configurations set

--- app/src/main/java/dev/olaore/learning_dagger/screens/common/viewmvcs/ViewMvcFactory.kt
+++ app/src/main/java/dev/olaore/learning_dagger/screens/common/viewmvcs/ViewMvcFactory.kt
@@ -6,19 +6,24 @@ import dev.olaore.learning_dagger.imageloader.ImageLoader
 import dev.olaore.learning_dagger.screens.questiondetails.QuestionDetailsViewMvc
 import dev.olaore.learning_dagger.screens.questionslist.QuestionsListViewMvc
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewMvcFactory @Inject
-    constructor(
-        private val layoutInflater: LayoutInflater,
-        private val imageLoader: ImageLoader
-    ) {
+constructor(
+    private val layoutInflaterProvider: Provider<LayoutInflater>,
+    private val imageLoaderProvider: Provider<ImageLoader>
+) {
 
     fun newQuestionListViewMvc(container: ViewGroup?): QuestionsListViewMvc {
-        return QuestionsListViewMvc(layoutInflater, container)
+        return QuestionsListViewMvc(layoutInflaterProvider.get(), container)
     }
 
     fun newQuestionDetailsViewMvc(container: ViewGroup?): QuestionDetailsViewMvc {
-        return QuestionDetailsViewMvc(layoutInflater, container, imageLoader)
+        return QuestionDetailsViewMvc(
+            layoutInflaterProvider.get(),
+            container,
+            imageLoaderProvider.get()
+        )
     }
 
 }
